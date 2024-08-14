11833607e86f1cb4ae394f96024b9fdcab58bd0c
your name <your email>
2024-08-14 15:44:18 +0100

    nav, home page

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,12 +3,13 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
 import { MdOutlineMenu } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
     const navLists = [
         {
             id: 1,
-            href: "#",
+            href: "/",
             title: "Home"
         },
         {
@@ -23,21 +24,22 @@ const Navbar = () => {
         },
         {
             id: 4,
-            href: "#",
+            href: "/signin",
             title: "Sign In"
         },
     ]
     let x = 3
+    const [navOpen, setNavOpen] = useState(false)
 
     
   return (
     <nav className='flex items-center justify-between p-3 shadow-md'>
-       <Link href={'#'} className='text-2xl text-blue-600 font-bold flex gap-1 items-center'>
+       <Link href={'/'} className='text-2xl text-blue-600 font-bold flex gap-1 items-center z-50'>
         <Image src={'/logo.png'} width={200} height={200} className='rounded-full w-14 h-14'/>
         <span>to-do</span>
        </Link>
        
-       <ul className='flex gap-14'>
+       <ul className={`flex gap-14 max-lg:flex-col  max-lg:justify-center max-lg:items-center max-lg:fixed max-lg:w-full max-lg:h-dvh max-lg:top-0 max-lg:right-0 max-lg:bg-slate-200/90 transition-all ${!navOpen ? "max-lg:translate-x-full" : ""} `}>
         {navLists.map((list)=>(
 
         <li key={list.id} className='font-semibold hover:text-blue-600 hover:border-b hover:border-b-blue-600'>
@@ -48,6 +50,12 @@ const Navbar = () => {
         ))}
        </ul>
       
+      <button
+        onClick={()=>setNavOpen(!navOpen)}
+        className='lg:hidden z-50'
+      >
+        {navOpen ? <IoMdClose className='text-2xl text-blue-600'/> : <MdOutlineMenu className='text-2xl text-blue-600'/>}
+      </button>
     </nav>
   )
 }
