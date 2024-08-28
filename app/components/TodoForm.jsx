ffda056a3e7d0b64455665b46697ca6a9d50ee87
Nwadike uchechukwu <uchechukwunwadike0@gmail.com>
2024-08-28 16:52:53 +0100

    changes

--- app/components/TodoForm.jsx
+++ app/components/TodoForm.jsx
@@ -1,35 +1,110 @@
-import { Field, Form, Formik } from 'formik'
-import React from 'react'
+"use client"
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import React, { useState } from 'react'
+import { FaPlus } from "react-icons/fa6";
 import * as Yup from 'yup'
+import { collection, addDoc, getDocs, QuerySnapshot } from 'firebase/firestore'
+import { db } from '@/lib/firebase';
+import { FaTrash } from "react-icons/fa";
+
 
 const TodoForm = () => {
-  const inival ={
-    title: '',
-    task: ''
+  const [todos, setTodos]=  useState([])
+  useEffect(()=>{
+    // function to fetch todos
+    const fecthTodos = async()=>{
+      try{
+          const querysnapshot = await getDocs(collection (db, 'todos'))
+          const todosData = querysnapshot.docs.map(doc =>({
+            id: doc.id,
+            ...doc.data()
+          }))
+      } catch(error){
+        console.error("Error fetching data:", error);
+        
+      }
+      
+      }
+    
+      }), []
+  const initVal = {
+    title: ''
   }
+
   const formValidation = Yup.object().shape({
     title: Yup.string()
-    .required('please enter a task')
+      .required('Please enter a task')
   })
 
-  const handleSubmit = (values)=>{
+  const handleSubmit = async (values) => {
     console.log(values);
     
+    // create a document to be stored
+    const info = {
+      title: values.title
+    }
+
+
+    // add this document to the database
+    const docRef = collection(db, "todos")
+    await addDoc(docRef, info)
   }
+
+  // fetch 
+
   return (
     <main className='h-dvh bg-blue-50'>
-        <Formik
-         initialValues={inival}
-         validationSchema={formValidation}
-        >
-            <Form>
-                <Field
-                 name='title'
-                 placeholder='enter a task'
-                />
-                
-            </Form>
-        </Formik>
+      <Formik
+        initialValues={initVal}
+        validationSchema={formValidation}
+        onSubmit={handleSubmit}
+      >
+        <Form className='flex justify-center'>
+          <div className='w-full p-10 flex items-center gap-10'>
+            <Field
+              name='title'
+              placeholder='Enter a task...'
+              className="w-full border-none outline-none rounded-md px-3 py-2  text-2xl"
+            />
+            <ErrorMessage
+              name='title'
+              component={'p'}
+              className='text-red-500 text-sm'
+            />
+            {/* <Field
+              name='task'
+              placeholder='Enter a task...'
+              className="w-full border-none outline-none rounded-md px-3 py-2  text-2xl"
+            /> */}
+
+            <button
+              type='submit'
+              className='p-3 rounded-md bg-white text-blue-600 text-2xl'
+            >
+              <FaPlus />
+            </button>
+          </div>
+        </Form>
+      </Formik>
+
+      <div>
+        <ul className='w-full'>
+          {
+            todos.map((todo)=>(
+              <li key={todo.id} className='w-[90%] mx-auto flex justify-between p-3 m-3 text-lg border-b border-b-gray-300 rounded-lg'>
+                <input type="checkbox" />
+                {todo.title}
+                <button className='hover:text-red-600 hover:text-2xl transition-all'>
+                  <FaTrash/>
+                </button>
+              </li>
+            ))
+          }
+
+        </ul>
+      <p className='flex justify-center'>You have 2 task</p>
+      </div>
+
     </main>
   )
 }
